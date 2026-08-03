bb535fe74a7e6a0f84e565169a28bc60bd007f73
avgx <agovorovsky@gmail.com>
2026-08-03 14:04:54 +0300

    add SignInButton

---
+++ Sources/OneAccountUI/Profile/SignInButton.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+@MainActor
+public struct SignInButton: View {
+    let onSignIn: () -> Void
+
+    public init(onSignIn: @escaping () -> Void) {
+        self.onSignIn = onSignIn
+    }
+
+    public var body: some View {
+        Button(L10n.string("sign-in"), action: onSignIn)
+    }
+}
